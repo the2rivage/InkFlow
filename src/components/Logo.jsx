1a338d8c27431d57e2afb3e5ba367a38b0373352
Sahil Bainya <sahilbainya2005@gmail.com>
2026-05-02 17:01:56 +0530

    fixed logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 export default function Logo() {
   const themeMode = useSelector((state) => state.theme.themeMode);
   const darkUrl =
-    "https://res.cloudinary.com/datjhn3ph/image/upload/w_150,h_70,c_fit/v1777540913/dark-Icon_umfiov.png";
+    "https://res.cloudinary.com/datjhn3ph/image/upload/w_150,h_70,c_fit/v1777721153/dark-Icon_umfiov.png";
   const lightUrl =
     "https://res.cloudinary.com/datjhn3ph/image/upload/w_150,h_70,c_fit/v1777545924/icon-lightTheme_t0hbwp.png";
   const currIcon = themeMode === "dark" ? darkUrl : lightUrl;
